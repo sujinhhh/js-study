a6a1cbf2a8b809253cfa2b97e130648475c6fb95
이수진_세부내용 작성 <email@sujinmail.com>
2021-02-08 19:31:01 -0500

    changing Background

--- practice/count-down-timer/script.js
+++ practice/count-down-timer/script.js
@@ -29,3 +29,21 @@ function formatTime(time) {
 countdown();
 
 setInterval(countdown, 1000);
+
+//  Chagne Background color
+
+const bgSelection = [
+  "https://images.unsplash.com/photo-1460295956739-9e21c5aabf8d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2279&q=80",
+  "https://images.unsplash.com/photo-1445331713779-ceec1147968f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+  "https://images.unsplash.com/photo-1477414348463-c0eb7f1359b6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+  "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+  "https://images.unsplash.com/photo-1564419431636-fe02f0eff7aa?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1562&q=80",
+  "https://images.unsplash.com/photo-1515545109095-cf8abbc557f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=944&q=80",
+  "https://images.unsplash.com/photo-1422207134147-65fb81f59e38?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1558&q=80",
+];
+const backgroundBtn = document.getElementById("bgButton");
+
+backgroundBtn.addEventListener("click", () => {
+  const randomNumber = Math.floor(Math.random() * bgSelection.length);
+  document.body.style.backgroundImage = `url(${bgSelection[randomNumber]})`;
+});
