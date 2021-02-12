1545d11a93232fae6bb77cef94fb2947c00085cf
이수진_세부내용 작성 <email@sujinmail.com>
2021-02-12 13:11:40 -0500

    donefunction in local

---
+++ shoppingCart/review.js
@@ -0,0 +1,13 @@
+// const body = document.body;
+
+// const div = document.querySelector("div");
+// const hi = document.querySelector("#hi");
+// const bye = document.querySelector("#bye");
+
+// hi.style.backgroundColor = "red";
+// body.style.backgroundColor = "#eeeeee";
+// div.style.display = "flex";
+// div.style.alignItems = "center";
+// div.style.justifyContent = "center";
+
+console.log(parseInt("88.4kgjk"));
